881150f1769ce775cce83e3c5bfd928cba51009e
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-03-03 05:06:46 +0200

    .

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api;
 
+use Carbon\CarbonInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,16 +17,29 @@ class UserResource extends JsonResource
             'phone' => $this->phone,
             'role' => $this->role,
             'national_id' => $this->national_id,
-            'birth_date' => $this->birth_date?->format('Y-m-d'),
+            'birth_date' => $this->formatDateValue($this->birth_date, 'Y-m-d'),
             'gender' => $this->gender,
             'address' => $this->address,
-            'email_verified_at' => $this->email_verified_at?->format('Y-m-d H:i:s'),
+            'email_verified_at' => $this->formatDateValue($this->email_verified_at, 'Y-m-d H:i:s'),
             'location' => new LocationResource($this->whenLoaded('location')),
             'family_members' => FamilyMemberResource::collection($this->whenLoaded('familyMembers')),
             'subscriptions' => SubscriptionResource::collection($this->whenLoaded('subscriptions')),
             'payments' => PaymentResource::collection($this->whenLoaded('payments')),
-            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+            'created_at' => $this->formatDateValue($this->created_at, 'Y-m-d H:i:s'),
+            'updated_at' => $this->formatDateValue($this->updated_at, 'Y-m-d H:i:s'),
         ];
+    }
+
+    private function formatDateValue(mixed $value, string $format): ?string
+    {
+        if (blank($value)) {
+            return null;
+        }
+
+        if ($value instanceof CarbonInterface) {
+            return $value->format($format);
+        }
+
+        return is_string($value) ? $value : null;
     }
 }
